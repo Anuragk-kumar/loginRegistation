1e5b100306f89a7a7b3548f10cb3ba3f01516543
Anurag kumar <74180720+Anuragk-kumar@users.noreply.github.com>
2023-08-11 07:41:19 +0530

    Update models.py

--- login/models.py
+++ login/models.py
@@ -1,1 +1,33 @@
+from django.db import models
+
+
+
+class UserProfile(models.Model):
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    username = models.CharField(max_length=20, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=25)
+    status = models.BooleanField(default=False)
+
+    @staticmethod
+    def get_user_by_username(username):
+        try:
+            return UserProfile.objects.get(username=username)
+        except:
+            return False
+
+
+    def isExists(self):
+        if UserProfile.objects.filter(username = self.username):
+            return True
+        return False
+
+    def register(self):
+        self.save()
+
+
+    def __str__(self):
+        return f"{self.first_name}"
+
 
